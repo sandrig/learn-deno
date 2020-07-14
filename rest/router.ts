ce80cc138f92385af86690b20bce1d158964f9ad
sandrig <sandrig.dev@gmail.com>
2020-07-14 09:23:38 +0300

    Learn the basics of REST API

---
+++ rest/router.ts
@@ -0,0 +1,19 @@
+import { Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  removeUser,
+} from "./controller.ts";
+
+const router = new Router();
+
+router
+  .get("/api/users", getUsers)
+  .get("/api/users/:id", getUser)
+  .post("/api/users", createUser)
+  .put("/api/users/:id", updateUser)
+  .delete("/api/users/:id", removeUser);
+
+export { router };
